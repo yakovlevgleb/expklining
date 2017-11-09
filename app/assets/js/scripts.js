56bcc1347a527ce84cdc1ef825b439c5a8a32b23
Yakovlev Gleb <brin@ya.ru>
2017-11-09 19:29:08 +0500

    end #2

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -1,11 +1,5 @@
-/*!
- * fastshell
- * Fiercely quick and opinionated front-ends
- * https://HosseinKarami.github.io/fastshell
- * @author Hossein Karami
- * @version 1.0.5
- * Copyright 2017. MIT licensed.
- */
+'use strict'
+
 $(document).ready(function() {
 
   $('.carousel').slick({
